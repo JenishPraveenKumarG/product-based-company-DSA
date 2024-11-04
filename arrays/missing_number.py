f8aff945b50f0d05611d01dc16cd85aabe299654
JenishPraveenKumarG <jenishpraveenkumar2911@gmail.com>
2024-11-04 11:34:35 +0530

    Updated intersection_of_sorted_arrays.py

--- arrays/missing_number.py
+++ arrays/missing_number.py
@@ -1,21 +1,55 @@
 # Brute force solution
 
-def missingNumber(self, nums: List[int]) -> int:
-    n = len(nums)
-    for i in range(n):
-        if i not in nums:
+def missingNumber(arr):
+    n = len(arr)
+    for i in range(1,n+1):
+        if i not in arr:
             return i
-    return i+1
+    return 0
+
+arr = [1,3,4,5]
+print(missingNumber(arr))
+
+# TC - O(n * w)
+# SC - O(1)
+
+
+
+# Better solution using hash array
+
+
+def missingNumber(a):
+    N = len(a)+1
+    hash = [0] * (N + 1)  # hash array
+
+    # storing the frequencies:
+    for i in range(N - 1):
+        hash[a[i]] += 1
+
+    # checking the frequencies for numbers 1 to N:
+    for i in range(1, N + 1):
+        if hash[i] == 0:
+            return i
+
+arr = [1,3,4,5]
+print(missingNumber(arr))
+
 
 
 # optimised solution using mathematics
 
-def missingNumber(self, nums: List[int]) -> int:
-    n = len(nums)
+def missingNumber(arr):
+    n = len(arr)
 
     sum = 0
-    for num in nums:
+    for num in arr:
         sum+=num
         
     n = (n*(n+1))//2
     return n-sum
+
+arr = [1,3,4,5]
+print(missingNumber(arr))
+
+# TC - O(n)
+# SC - O(1)
