87ba4d6e13df35a6251514c7727fee5a9079e63f
JenishPraveenKumarG <jenishpraveenkumar2911@gmail.com>
2024-11-04 14:15:35 +0530

    Updated two_sum.py

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -19,3 +19,27 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             return [i,dict[req]]
         dict[nums[i]] = i
     return []
+
+# TC - O(n)
+# Sc - O(n)
+
+# Optimal solution using 2 pointers
+
+def two_sum(arr,target):
+    l = 0
+    r = len(arr)-1
+    while l<=r:
+        if arr[l] + arr[r]== target:
+            return [l,r]
+        elif arr[l]+arr[r] < target:
+            l+=1
+        else:
+            r-=1
+    return []
+
+arr = [2,5,6,8,11]
+target = 14
+print(two_sum(arr,target))
+
+# TC - O(n)
+# SC - O(1)
