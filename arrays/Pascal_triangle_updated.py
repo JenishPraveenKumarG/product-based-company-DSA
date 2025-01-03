74d5433426a5e38cd390059d73ec45d3e35ec428
Jenish Praveen Kumar G <137860558+JenishPraveenKumarG@users.noreply.github.com>
2025-01-03 23:45:50 +0530

    Update Pascal_triangle_updated.py

--- arrays/Pascal_triangle_updated.py
+++ arrays/Pascal_triangle_updated.py
@@ -1,20 +1,21 @@
 def pascal(num_row):
-  first_row = 1
+  first_row = [1]
   rows = [first_row]
 
   for _ in range(1,num_row):
     row = [1]
-    for i in range(1,len(rows[-1]):
-      num = row[-1][i-1] + [-1][i]
+    for i in range(1,len(rows[-1])):
+      num = rows[-1][i-1] + rows[-1][i]
       row.append(num)
-    row.appemd(1)
+    row.append(1)
     rows.append(row)
     
-return rows
+  return rows
 
 n = 5
 print(pascal(n))
 
 
     
-  
+# TC - O(n x n)
+# SC - O(n x n)
