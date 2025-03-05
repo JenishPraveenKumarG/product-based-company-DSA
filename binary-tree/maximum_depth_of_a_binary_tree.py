c66a23a46f937ad28a303a7d8d15f1f63b7d0a69
Jenish Praveen Kumar G <137860558+JenishPraveenKumarG@users.noreply.github.com>
2025-03-05 23:41:00 +0530

    Update maximum_depth_of_a_binary_tree.py

--- binary-tree/maximum_depth_of_a_binary_tree.py
+++ binary-tree/maximum_depth_of_a_binary_tree.py
@@ -9,7 +9,7 @@ def max_depth(root):
         return 0
     left = max_depth(root.left)
     right = max_depth(root.right)
-    return 1+max(left,right)
+    return 1+max(left,right) 
 
     # TC - O(n)
     # SC - O(n)
